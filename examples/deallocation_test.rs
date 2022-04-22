b2fda7916035f36070bc9921276af0a041ad8a4c
Niels Bonten <42673857+Niebon@users.noreply.github.com>
2022-04-22 10:43:33 +0200

    Deallocate circular references & additions to external library (#4)
    * - Implemented Drop trait for PointIndexation to deallocate circular references that arise from 'point -> neighbours -> point -> ...' sequences. This is implemented for hnsw<T, D> with all T,D combinations.
    - Added constructors to the external library to give max elements and max layers as arguments for hnsw_u16 and hnsw_f32.
    - Added drop procedures to the external library for hnsw_u16 and hnsw_f32.
    - Added Levenshtein metric as possible setting for hnsw_u16 constructors in external library.
    
    * - Improved comment.
    
    Co-authored-by: niels <niels@sannsyn.com>

---
+++ examples/deallocation_test.rs
@@ -0,0 +1,36 @@
+use serde::de::Unexpected::Map;
+use hnsw_rs::dist::DistL1;
+use hnsw_rs::hnsw::Hnsw;
+
+// A test program to see if memory from insertions gets deallocated.
+// This program sets up a process that iteratively builds a new model and lets it go out of scope.
+// Since the models go out of scope, the desired behavior is that memory consumption is constant while this program is running.
+fn main() {
+    let mut counter: usize = 0;
+    loop {
+        let hnsw: Hnsw<f32, DistL1> = Hnsw::new(
+            15,
+            100_000,
+            20,
+            500_000,
+            DistL1 {}
+        );
+        let s1 = [1.0, 0.0, 0.0, 0.0];
+        hnsw.insert_slice((&s1, 0));
+        let s2 = [0.0, 1.0, 1.0];
+        hnsw.insert_slice((&s2, 1));
+        let s3 = [0.0, 0.0, 1.0];
+        hnsw.insert_slice((&s3, 2));
+        let s4 = [1.0, 0.0, 0.0, 1.0];
+        hnsw.insert_slice((&s4, 3));
+        let s5 = [1.0, 1.0, 1.0];
+        hnsw.insert_slice((&s5, 4));
+        let s6 = [1.0, -1.0, 1.0];
+        hnsw.insert_slice((&s6, 5));
+
+        if counter % 1_000_000 == 0 {
+            println!("{}", counter)
+        }
+        counter = counter + 1;
+    }
+}
