a3f9fcec28631d44de995a30b8a30c69a937643c
Petter <petter.egesund@gmail.com>
2023-03-31 11:51:02 +0200

    code cleanup in filter example

--- examples/levensthein.rs
+++ examples/levensthein.rs
@@ -3,10 +3,8 @@ use hnsw_rs::prelude::*;
 use hnsw_rs::dist::{DistLevenshtein};
 use rand::Rng;
 use std::iter;
-use std::time::{Instant};
 
-
-
+// Used to create a random string
 fn generate(len: usize) -> String {
     const CHARSET: &[u8] = b"abcdefghij";
     let mut rng = rand::thread_rng();
@@ -14,38 +12,40 @@ fn generate(len: usize) -> String {
     iter::repeat_with(one_char).take(len).collect()
 }
 
-fn search(word: &str, hns: &Hnsw<u16, DistLevenshtein>, words: &Vec<String>) {
+// this function uses a sorted vector as a filter
+fn search_vector_filter(word: &str, hns: &Hnsw<u16, DistLevenshtein>, words: &Vec<String>) {
+    // Create a sorted vector of ids
+    // the ids in the vector will be used as a filter
     let mut vec: Vec<u16> = Vec::new();
     for c in word.chars() {
         vec.push(c as u16);
     }
-    let ef_search = 30;
-    let start = Instant::now();
+
     let mut filter: Vec<usize> = Vec::new();
     for i in 300..400 {
         filter.push(i);
     }
-    let mut filter2: Vec<usize> = Vec::new();
-    for i in 300..400 {
-        filter2.push(i);
-    }
-    // let res = hns.search(&vec, 50, ef_search);
+
+    // Now let us do the search
+    // ids not in the vector will not be indluced in the search results
+    let ef_search = 30;
     let res = hns.search_possible_filter(&vec, 10, ef_search, Some(&filter));
-    let duration = start.elapsed();
-    println!("Time elapsed in expensive_function() is: {:?}", duration);
+
     let mut i = 0;
     for r in res {
         i = i + 1;
         println!("{:?}  Word: {:?} Id: {:?} Distance: {:?}", i, words[r.d_id], r.d_id, r.distance);
     }
+
+    // Then do a "normal search without filter"
     println!("==========");   
     let res3 = hns.search_possible_filter(&vec, 10, ef_search, None);
     for r in res3 {
         println!("Word: {:?} Id: {:?} Distance: {:?}", words[r.d_id], r.d_id, r.distance);
     }
 
-
 }
+
 
 fn main() {
 
@@ -72,6 +72,6 @@ fn main() {
             println!("Inserting: {:?}", i);
         }
     }
-    search(&"abcde", &hns, &words);
+    search_vector_filter(&"abcde", &hns, &words);
 
 }
